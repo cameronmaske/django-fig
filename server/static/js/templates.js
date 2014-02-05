256e9218e5b4a75625ca86cc7f778f1a6d105946
Cameron Maske <cameronmaske@gmail.com>
2014-02-05 10:50:56 +0000

    Added in Foundation SCSS framework. Updated todoList template to use the new styles.
    Closes #5

--- server/static/js/templates.js
+++ server/static/js/templates.js
@@ -6,6 +6,6 @@ try {
 }
 module.run(['$templateCache', function($templateCache) {
   $templateCache.put('todoList.template',
-    '<div><span>{{remaining()}} of {{todos.length}} remaining</span> [ <a href="" ng-click="archive()">archive</a> ]<ul class="unstyled"><li ng-repeat="todo in todos"><input type="checkbox" ng-model="todo.done"><span class="done-{{todo.done}}">{{todo.text}}</span></li></ul><form ng-submit="addTodo()"><input type="text" ng-model="todoText" size="30" placeholder="add new todo here"><input class="btn-primary" type="submit" value="add"></form></div>');
+    '<div class="row panel todo-panel"><div class="large-8 columns"><h2 class="subheader">{{remaining()}} of {{todos.length}} remaining</h2></div><div class="large-4 columns"><button class="button tiny todo-archive" ng-click="archive()">Archive</button></div><div class="large-12 columns" ng-repeat="todo in todos"><input type="checkbox" ng-model="todo.done"><label class="done-{{todo.done}}">{{todo.text}}</label></div><form class="large-12 columns" ng-submit="addTodo()"><input type="text" ng-model="todoText" size="30" placeholder="add new todo here"><input class="button" type="submit" value="Add"></form></div>');
 }]);
 })();
